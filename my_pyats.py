76d0361dd97ca57731ab54d436f0e8f14fed7a87
Jay Taylor <foosyou@gmail.com>
2019-10-12 22:23:20 -0500

    mutli devices hate me

--- my_pyats.py
+++ my_pyats.py
@@ -1,13 +1,16 @@
 from genie.testbed import load
 from genie.conf.base.device import Device
+import logging
+
 
 tb = load('my_testbed.yaml')
 
 for dev in tb.devices:
     print(dev)
     device = tb.devices[dev]
-    device.connect()
+    device.connect(log_stdout=False)
+
     sh_ip_int = device.parse('show ip interface brief')
-    print(sh_ip_int)
+#    print(sh_ip_int)
 
 
