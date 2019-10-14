1b601e48a031947b68be7d5a0f8ac611ce52dd6d
Jay Taylor <foosyou@gmail.com>
2019-10-13 21:01:18 -0500

    mult devices, check int up > IP

--- my_pyats.py
+++ my_pyats.py
@@ -2,7 +2,7 @@ from genie.testbed import load
 from genie.conf.base.device import Device
 import logging
 
-
+#
 tb = load('my_testbed.yaml')
 
 for dev in tb.devices:
@@ -11,6 +11,7 @@ for dev in tb.devices:
     device.connect(log_stdout=False)
 
     sh_ip_int = device.parse('show ip interface brief')
-    print(sh_ip_int)
-
-
+    
+    for int in sh_ip_int['interface']:
+        if sh_ip_int['interface'][int]['status'] == 'up':
+            print(f"{int}: {sh_ip_int['interface'][int]['ip_address']}")
