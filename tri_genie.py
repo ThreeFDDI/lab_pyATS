d29050e12ecf33238e078141e315c90b0f443c88
JT <JT@ThreeFDDI.com>
2019-10-14 11:44:22 -0500

    pre-post stuff

--- tri_genie.py
+++ tri_genie.py
@@ -3,8 +3,10 @@ from genie.testbed import load
 from genie.conf.base.device import Device
 from genie.utils.diff import Diff
 
+pre_post = input("Please enter 'pre' or 'post' for testing phase: ")
+
 # load testbed file
-tb = load('my_testbed.yaml')
+tb = load('tri_testbed.yaml')
 
 # init routes list
 routes = []
@@ -17,16 +19,24 @@ for dev in tb.devices:
     device = tb.devices[dev]
     # disable logging
     device.connect(log_stdout=False)
+    
+    routes = device.parse('show ip route')
+    
+    with open(f"{dev}-{pre_post}.info", "w+") as f:
+        f.write(str(routes))
 
+    if pre_post == 'post':
+        pass
+        # Diff routing tables   
+        #print("Diff:")
+        #diff = Diff(routes[0], routes[1])
+        #diff.findDiff()
+        #print(diff)
+
+    
     # send command and parse output
-    routes.append(device.parse('show ip route vrf Mgmt-intf'))
+    #routes.append()
 
     # print device routing table
-    print(routes)
     print()
 
-# Diff routing tables   
-print("Diff:")
-diff = Diff(routes[0], routes[1])
-diff.findDiff()
-print(diff)
