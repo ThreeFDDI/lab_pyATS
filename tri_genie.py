b02ffd2fe25740227be2a4c782aaaa24c98b05e5
JT <JT@ThreeFDDI.com>
2019-10-14 14:33:25 -0500

    comments and filenames

--- tri_genie.py
+++ tri_genie.py
@@ -4,7 +4,7 @@ from genie.conf.base.device import Device
 from genie.utils.diff import Diff
 
 # init variable to control pre / post testing
-pre_post = None
+pre_post = "post"
 
 # prompt user to set variable to control pre / post testing
 while pre_post not in ["pre", "post"]:
@@ -19,27 +19,28 @@ for dev in tb.devices:
     device = tb.devices[dev]
     # disable logging
     device.connect(log_stdout=False)
-    
+    # grab routing table from each device
     routes = device.parse('show ip route')
-    
-    with open(f"{dev}-{pre_post}.info", "w+") as f:
+    # save routing table output to file
+    with open(f"{dev}_{pre_post}.info", "w+") as f:
         json.dump(routes, f)
 
+    # additional processing for post testing
     if pre_post == 'post':
-        pass
-        with open(f"{dev}-pre.info", "r") as f:
+        # load pre-change routing table from file
+        with open(f"{dev}_pre.info", "r") as f:
             pre = json.load(f)
-        with open(f"{dev}-post.info", "r") as f:
+            
+        # load post-change routing table from file
+        with open(f"{dev}_post.info", "r") as f:
             post = json.load(f)
         
-        # Diff routing tables   
-        print(f"{dev} diff:")
-        print("~"*10)
-        # set diff variable and exlude "updated" field
+        # set diff variables and exlude "updated" field
         diff = Diff(pre, post, exclude="updated")
         # diff pre/post files
         diff.findDiff()
-        # print dif
-        print(diff)
-        print()
-
+        # write diff to file
+        with open(f"{dev}.diff", "w+") as f:
+            f.write(str(diff))
+        # print message for end of device processing
+        print(f"{dev} diff results written to file.")
